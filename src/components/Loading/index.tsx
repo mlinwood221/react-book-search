bed7235d8786b85fcafc94f0475fb6bdb9ac2fb7
mlinwood221 <linwoodmatthews221@gmail.com>
2019-08-16 00:00:13 +0200

    Improve loading component styles

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
+import loadingIcon from '@mdi/svg/svg/loading.svg';
 
-const Loading = () => <h2>Loading...</h2>;
+import './style.scss';
+
+const Loading = () => (
+  <div className="loading">
+    <svg viewBox={loadingIcon.viewBox} className="loading__icon">
+      <use xlinkHref={`#${loadingIcon.id}`} />
+    </svg>
+    <p className="loading__message">Loading...</p>
+  </div>
+);
 
 export default Loading;
